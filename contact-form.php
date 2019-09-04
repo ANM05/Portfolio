1e39442e251bac0091898a7b22693c94d92db4c6
Antony Nganga <antonynganga05@gmail.com>
2019-09-04 21:01:14 +0300

    Add contact form handler php

---
+++ contact-form.php
@@ -0,0 +1,22 @@
+<?php
+ $name = $_POST['name'];
+ $user_email = $_POST['email'];
+ $message = $_POST['message'];
+
+ $email_from = 'https://anm05.github.io/Portfolio/';
+ $email_subject = 'Enquery form submission';
+
+ $email_body = "User name: $name.\n".
+                "User email: $user_email.\n".
+                  "Message: $message.\n";
+
+ $to = 'antonynganga05@gmail.com';
+ 
+ $headers = "From: $email_from \r\n";
+ $headers .= "Reply-To: $user_email \r\n";
+
+ mail($to,$email_subject,$email_body,$headers);
+
+ header("Location: index.html");
+ 
+?>
